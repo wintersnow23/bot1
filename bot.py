9436db191c1a477ac5b673b3ebd1f133aacbae0d
wintersnow23 <45384724+wintersnow23@users.noreply.github.com>
2018-12-04 16:58:47 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,24 +1,39 @@
-import discord
+import random
+import asyncio
+import aiohttp
+import json
+from discord import Game
+from discord.ext.commands import Bot
+
 
 TOKEN = 'NTE2ODgyOTExODI0MzE0MzY4.Dt6I9A.F_bMDgeE8XxVmt0UBvduX0GlLRg'
 
-client = discord.Client()
+startup_extensions = ["music"]
+BOT_PREFIX = ("?", "!")
 
-@client.event
-async def on_message(message):
-    # we do not want the bot to reply to itself
-    if message.author == client.user:
-        return
+client = Bot(command_prefix=BOT_PREFIX)
 
-    if message.content.startswith('!hello'):
-        msg = 'Hello {0.author.mention}'.format(message)
-        await client.send_message(message.channel, msg)
+@client.command(name='8ball',
+                description="Answers a yes/no question.",
+                brief="Answers from the beyond.",
+                aliases=['eight_ball', 'eightball', '8-ball'],
+                pass_context=True)
+async def eight_ball(context):
+    possible_responses = [
+        'That is a resounding no',
+        'It is not looking likely',
+        'Too hard to tell',
+        'It is quite possible',
+        'Definitely',
+    ]
+    await client.say(random.choice(possible_responses) + ", " + context.message.author.mention)
 
-@client.event
-async def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print('------')
+
+@client.command()
+async def square(number):
+    squared_value = int(number) * int(number)
+    await client.say(str(number) + " squared is " + str(squared_value))
+
+
 
 client.run(TOKEN)
